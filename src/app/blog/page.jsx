e7bf99fa96ef44bf4e9194e2b47b115acce99cc2
daudi13 <davouma13@gmail.com>
2023-11-24 21:18:42 +0300

    Add blogspot

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,107 @@
 import React from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.maincontainer}>
+      <Link className={styles.container} href="/blog/1">
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.unsplash.com/photo-1682687220211-c471118c9e92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.contentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque? Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque?
+          </p>
+        </div>
+      </Link>
+      <Link className={styles.container} href="/blog/2">
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.unsplash.com/photo-1682687220211-c471118c9e92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.contentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque? Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque?
+          </p>
+        </div>
+      </Link>
+      <Link className={styles.container} href="/blog/3">
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.unsplash.com/photo-1682687220211-c471118c9e92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.contentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque? Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque?
+          </p>
+        </div>
+      </Link>
+      <Link className={styles.container} href="/blog/3">
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.unsplash.com/photo-1682687220211-c471118c9e92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.contentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque? Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque?
+          </p>
+        </div>
+      </Link>
+      <Link className={styles.container} href="/blog/4">
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.unsplash.com/photo-1682687220211-c471118c9e92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.contentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque? Quisquam
+            voluptates, quibusdam, quia, voluptatum voluptate quos quod
+            voluptatibus officiis numquam aliquid doloremque?
+          </p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import styles from "./page.module.css"
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
-const portolioCategory = () => {
+const portolioCategory = ({ params }) => {
+  console.log(params)
+
   return (
-    <div>portolioCategory</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.contentTitle}>The Art of App Development</h1>
+          <p className={styles.desc}>
+            In the dynamic landscape of technology, application development has become a cornerstone for businesses and individuals alike. Whether you're crafting a mobile app to streamline daily tasks or developing a robust web application for a global audience, the process requires a blend of creativity, strategic thinking, and technical prowess.
+
+          </p>
+          <Button text="see more" url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/4974912/pexels-photo-4974912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.contentTitle}>Conceptualization and Planning</h1>
+          <p className={styles.desc}>
+            Before diving into code, a solid understanding of the application's purpose, target audience, and key features is essential. This phase involves brainstorming ideas, creating wireframes, and outlining the project's scope. Clear goals and a well-defined roadmap set the foundation for a successful development journey.
+          </p>
+          <Button text="see more" url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/1181244/pexels-photo-1181244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
